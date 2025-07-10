2068f258a77bed57cb18633a989d9dde22f92dec
yogeswarreddy939 <chennuruyogi2003@gmail.com>
2025-07-10 07:53:38 +0530

    Update traffic_env.py

--- traffic_env.py
+++ traffic_env.py
@@ -1,1 +1,23 @@
+import random
+
+class TrafficEnvironment:
+    def __init__(self):
+        self.state = 0  # 0 = low congestion, 1 = medium, 2 = high
+
+    def get_state(self, vehicle_count):
+        if vehicle_count < 5:
+            return 0
+        elif vehicle_count < 15:
+            return 1
+        else:
+            return 2
+
+    def get_reward(self, state, action):
+        # action: 0 = short green, 1 = long green
+        if state == 2 and action == 1:
+            return 10  # long green when congestion is high
+        elif state == 0 and action == 0:
+            return 8   # short green when traffic is light
+        else:
+            return -5  # bad decision
 
